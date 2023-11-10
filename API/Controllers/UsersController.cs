05421338ebf023265fa737a7a6d2870a8defe6c6
inv-Ayiba <tariogounga@gmail.com>
2023-11-10 06:46:36 +0100

    1h

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")] //api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context )
+        {
+            _context = context;
+            
+        }
+        //Synchronous
+        // [HttpGet]
+        // public ActionResult<IEnumerable<AppUser>> GetUsers()
+        // {
+        //     var users = _context.Users.ToList();
+        //     return users;
+        // }
+        // [HttpGet("{id}")]
+        // public ActionResult<AppUser> GetUser(int id)
+        // {
+        //     return _context.Users.Find(id);
+        // }
+
+         [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return  users;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return  await _context.Users.FindAsync(id);
+        }
+    }
