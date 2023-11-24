2fab9c83e6f1e9ea4ebe62f39dfe8cd93eba6dc1
inv-Ayiba <tariogounga@gmail.com>
2023-11-24 08:20:45 +0100

    observables yippi

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,14 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
  baseUrl = 'https://localhost:5001/api/';
+ // so nav comp is'nt the only one that can track login/out brlow
+private currentUserSource = new BehaviorSubject<User | null>(null);
+currentUser$ = this.currentUserSource.asObservable();
   constructor(private http: HttpClient) { }
 
   login(model: any){
-    return this.http.post(this.baseUrl + 'account/login', model)
+    // why do we need this pipe again?
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User)=> {
+        const user = response;
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user))
+        }
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('user');
   }
 }
