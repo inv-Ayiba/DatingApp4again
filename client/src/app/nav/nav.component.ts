1e19e92848fcc82625d7c332380e260cfd5161e3
inv-Ayiba <tariogounga@gmail.com>
2023-11-24 10:54:21 +0100

    getCurrentUser(){

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,19 +10,19 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit{
   model: any = {};
-  loggedIn = false;
+  currentUser$: Observable<User | null> = of(null);
   constructor(private accountService: AccountService){}
 
   ngOnInit(): void {
-
+    this.currentUser$ = this.accountService.currentUser$ ;
   }
 
-  getCurrentUser(){
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    })
-  }
+  // getCurrentUser(){
+  //   this.accountService.currentUser$.subscribe({
+  //     next: user => this.loggedIn = !!user,
+  //     error: error => console.log(error)
+  //   })
+  // }
 
   login(){
     this.accountService.login(this.model).subscribe({
