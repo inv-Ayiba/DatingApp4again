268f38827d0ce5c75528d3fc21c8df7c0c38c9da
inv-Ayiba <tariogounga@gmail.com>
2023-11-24 10:32:24 +0100

    getCurrentUser(){ in nav.component

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -19,13 +19,17 @@ currentUser$ = this.currentUserSource.asObservable();
       map((response: User)=> {
         const user = response;
         if(user){
-          localStorage.setItem('user', JSON.stringify(user))
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user)
         }
       })
     )
   }
-
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
   logout() {
     localStorage.removeItem('user');
+    this.currentUserSource.next(null)
   }
 }
