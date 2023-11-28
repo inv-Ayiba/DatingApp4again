8884d6c3181330f78fe9007a263a63c0d72442e4
inv-Ayiba <tariogounga@gmail.com>
2023-11-28 10:40:44 +0100

    ssd change

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +13,7 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit{
   model: any = {};
   // currentUser$: Observable<User | null> = of(null);
-  constructor(public accountService: AccountService){}
+  constructor(public accountService: AccountService, private router: Router,private toastr: ToastrService){}
 
   ngOnInit(): void {
     // this.currentUser$ = this.accountService.currentUser$ ;
@@ -27,15 +29,16 @@ export class NavComponent implements OnInit{
   login(){
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response);
+        this.router.navigateByUrl('/members')
         // this.loggedIn= true;
       },
-      error: error => console.log("error oh",error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
     // this.loggedIn = false;
   }
 
