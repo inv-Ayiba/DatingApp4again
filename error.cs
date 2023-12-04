5dee7ece52d6975002d49cf3d7481a8d69d6c029
inv-Ayiba <tariogounga@gmail.com>
2023-12-04 20:21:28 +0100

    error

---
+++ error.cs
@@ -0,0 +1,4 @@
+// Node.js version v21.3.0 detected.
+// Odd numbered Node.js versions will not enter LTS status and should not be used for production. For more information, please see https://nodejs.org/en/about/previous-releases/.
+// Skipping installation: Package already installed
+// Package "ngx-bootstrap" was found but does not support schematics.
